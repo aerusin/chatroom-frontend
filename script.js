6143563f1cf24c0b28c054db2d1950600ab0b902
Alexander Rusin <arusin@udel.edu>
2023-10-02 21:50:12 -0400

    added login and logout

--- script.js
+++ script.js
@@ -12,6 +12,80 @@ firebase.initializeApp(firebaseConfig);
 const db = firebase.database();
 const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
+firebase.auth().onAuthStateChanged((user) => {
+  const loggedInContent = document.getElementById("logged-in-content");
+  const loggedOutContent = document.getElementById("logged-out-content");
+
+  if (user) {
+    loggedInContent.classList.remove("hidden");
+    loggedOutContent.classList.add("hidden");
+  } else {
+    loggedInContent.classList.add("hidden");
+    loggedOutContent.classList.remove("hidden");
+  }
+});
+
+document.getElementById("loginButton").addEventListener("click", () => {
+  const email = document.getElementById("email").value.trim();
+  const password = document.getElementById("password").value.trim();
+  firebase
+    .auth()
+    .signInWithEmailAndPassword(email, password)
+    .then((userCredential) => {
+      var user = userCredential.user;
+    })
+    .catch((error) => {
+      document.getElementById("authError").textContent = error.message;
+    });
+});
+
+document.getElementById("signupButton").addEventListener("click", () => {
+  const email = document.getElementById("email").value.trim();
+  const password = document.getElementById("password").value.trim();
+  firebase
+    .auth()
+    .createUserWithEmailAndPassword(email, password)
+    .then((userCredential) => {
+      var user = userCredential.user;
+      console.log(user);
+    })
+    .catch((error) => {
+      document.getElementById("authError").textContent = error.message;
+    });
+});
+
+document.getElementById("logoutButton").addEventListener("click", () => {
+  firebase.auth().signOut();
+});
+
+const appState = {
+  currentRoomID: null,
+};
+
+const roomsRef = db.ref("rooms");
+
+roomsRef.on("value", (snapshot) => {
+  const roomsData = snapshot.val();
+  if (roomsData) {
+    const rooms = Object.keys(roomsData);
+    displayRooms(rooms);
+  }
+});
+
+const displayRooms = (rooms) => {
+  console.log(rooms);
+  const roomsList = document.getElementById("joinable-rooms");
+  rooms.forEach((room) => {
+    const roomElement = document.createElement("p");
+    roomElement.textContent = room;
+    roomElement.addEventListener("click", () => {
+      joinRoom(room);
+      console.log(room);
+    });
+    roomsList.appendChild(roomElement);
+  });
+};
+
 const checkRoom = () => {
   const roomID = document.getElementById("room-id").value.trim();
   const roomRef = db.ref("rooms/" + roomID);
@@ -25,7 +99,6 @@ const checkRoom = () => {
       document.getElementById(
         "room-status"
       ).textContent = `You are now in room ${roomID}`;
-      document.getElementById("homepage").classList.add("hidden");
       joinRoom(roomID);
     } else {
       document.getElementById("room-status").textContent = "Room not found!";
@@ -33,7 +106,33 @@ const checkRoom = () => {
   });
 };
 
-const joinRoom = (roomID) => {};
+const joinRoom = (roomID) => {
+  appState.currentRoomID = roomID;
+  document.getElementById("homepage").classList.add("hidden");
+  const roomRef = db.ref("rooms/" + roomID);
+  document.getElementById("room-display").classList.remove("hidden");
+  document.getElementById(
+    "room-header"
+  ).textContent = `Welcome to Room ${roomID}`;
+  const messagesRef = db.ref("rooms/" + roomID + "/messages");
+  messagesRef.on("child_added", (snapshot) => {
+    console.log(snapshot.val());
+    const messageData = snapshot.val();
+    displayMessage(messageData);
+  });
+};
+
+const displayMessage = (messageData) => {
+  const chatWindow = document.getElementById("room-messages");
+  const messageElement = document.createElement("p");
+  const date = new Date(messageData.timestamp);
+  const timeString = date.toLocaleTimeString();
+
+  messageElement.textContent = `${timeString} - ${
+    firebase.auth().currentUser.email
+  } - ${messageData.message}`;
+  chatWindow.appendChild(messageElement);
+};
 
 const generateID = () => {
   let result = "";
@@ -59,4 +158,16 @@ const createRoom = () => {
     .catch((error) => {
       console.error();
     });
+};
+
+const sendMessage = () => {
+  const messageRef = db.ref("rooms/" + appState.currentRoomID + "/messages");
+  const message = document.getElementById("message-input").value.trim();
+  console.log("firebae", firebase.auth());
+  messageRef.push({
+    user: firebase.auth().currentUser.email,
+    message: message,
+    timestamp: firebase.database.ServerValue.TIMESTAMP,
+  });
+  document.getElementById("message-input").value = "";
 };
